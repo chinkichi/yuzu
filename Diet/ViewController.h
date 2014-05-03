a274b8587601386cba38b9720b0fae2959a3f401
藤浦ゆず <fujiurahiroshi@fujiuraatsushi-no-MacBook-Air.local>
2014-05-03 13:56:59 +0900

    Initial Commit

---
+++ Diet/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Diet
+//
+//  Created by 藤浦ゆず on 2014/05/03.
+//  Copyright (c) 2014年 藤浦ゆず. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
